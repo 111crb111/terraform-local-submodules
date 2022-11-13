795765ea6bb9845975caf5e6b5faf29a8895f714
Yaroslav Subota <111CRB111@gmail.com>
2022-11-13 12:49:17 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,22 @@ module "submodule1" {
   source = "./modules/submodule1"
 }
 
-output "test_output" {
+module "submodule2" {
+  source = "./modules/submodule1"
+}
+
+module "submodule3" {
+  source = "./modules/submodule1"
+}
+
+output "test_output1" {
   value = module.submodule1.hello
+}
+
+output "test_output2" {
+  value = module.submodule2.hello
+}
+
+output "test_output3" {
+  value = module.submodule3.hello
 }
