9b7f679450f294a33ade80af2e02a665ed48c5c5
Yaroslav Subota <111CRB111@gmail.com>
2022-11-13 13:47:46 +0200

    Update main.tf

--- modules/submodule1/main.tf
+++ modules/submodule1/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "3.1.1"
-    }
-  }
-}
-
-provider "null" {
-  # Configuration options
-}
-
 resource "null_resource" "resource1" {
   count = 1
   provisioner "local-exec" {
